9e723b87d07fa9bf62cfb6c3fc78061a2fc6ac61
Jonathan Querubina <jotadev@me.com>
2025-02-10 15:30:56 -0300

    start command buttons and finished playbackRate

--- src/pages/player.js
+++ src/pages/player.js
@@ -3,7 +3,8 @@ import { useParams } from "react-router-dom";
 
 import styled from "styled-components";
 
-import { Slider } from "antd";
+import { Slider, Dropdown } from "antd";
+import { PauseOutlined, CaretRightOutlined } from '@ant-design/icons';
 
 import Api from "../libs/api";
 
@@ -44,6 +45,48 @@ const ControlsHolder = styled.div `
   align-items: center;
   justify-content: space-between;
   height: 60px;
+`;
+
+const ControlBlock = styled.div `
+  width: 100%;
+  height: 60px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  &.right {
+    justify-content: end;
+    padding-right: 24px;
+  }
+  &.left {
+    justify-content: start;
+    padding-left: 24px;
+  }
+`;
+
+const ControlBlockButton = styled.div `
+  border: 1px solid rgba(255,255,255,0.5);
+  width: 40px;
+  height: 24px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 10px;
+  color: rgba(255,255,255,1);
+  cursor: pointer;
+  border-radius: 6px;
+  margin-left: 24px;
+`;
+
+const PlayPauseButton = styled.div `
+    width: 24px;
+    height: 24px;
+    cursor: pointer;
+
+    svg {
+      width: 24px;
+      height: 24px;
+    }
 `;
 
 const ProgressHolder = styled.div `
@@ -90,7 +133,7 @@ export default function Player() {
     const [height, setHeight] = useState(window.innerHeight);
     const [width, setWidth] = useState(window.innerWidth);
 
-    const [loading, setLoading] = useState(false);
+    // const [loading, setLoading] = useState(false);
     const [media, setMedia] = useState();
 
     // video
@@ -99,14 +142,32 @@ export default function Player() {
     const [isPlaying, setIsPlaying] = useState(false);
 
     const [currentTime, setCurrentTime] = useState(0);
+    const [volume, setVolume] = useState(1);
     const [duration, setDuration] = useState(0);
 
 
-
+    const playPauseAction = () => {
+        if (!videoRef.current.paused) {
+            videoRef.current.pause();
+            setIsPlaying(false);
+        } else {
+            videoRef.current.play();
+            setIsPlaying(true);
+        }
+    }
 
     const changeCurrentTime = (time) => {
         videoRef.current.currentTime = time;
         setCurrentTime(time);
+    }
+
+    const changeVolume = (volume) => {
+        videoRef.current.volume = volume;
+        setVolume(volume);
+    }
+
+    const displayVolume = (volume) => {
+        return `${parseInt(volume*100)}%`;
     }
 
     const sec2Time = (sec) => {
@@ -119,6 +180,10 @@ export default function Player() {
     useEffect(() => {
         const video = videoRef.current;
         if (!video) return;
+
+        videoRef.current.volume = 1;
+        setVolume(1);
+
         const updateTime = () => setCurrentTime(video.currentTime);
         const updateDuration = () => setDuration(video.duration);
         video.addEventListener("timeupdate", updateTime);
@@ -134,6 +199,37 @@ export default function Player() {
             setCurrentTime(media.progress)
         }
     }, [media]);
+
+    const [currentPlaybackRate, setCurrentPlaybackRate] = useState(1);
+    const playbackRate = [{
+            key: '5',
+            label: '2x'
+        },
+        {
+            key: '4',
+            label: '1.75x'
+        },
+        {
+            key: '3',
+            label: '1.5x'
+        },
+        {
+            key: '2',
+            label: '1.25x'
+        },
+        {
+            key: '1',
+            label: '1x'
+        }
+    ];
+
+    const changePlaybackRate = (data) => {
+        const selected = parseInt(data.key);
+        const values = [0, 1, 1.25, 1.5, 1.75, 1.75, 2];
+        const finalPlaybackRate = values[selected];
+        videoRef.current.playbackRate = finalPlaybackRate;
+        setCurrentPlaybackRate(`${finalPlaybackRate}`);
+    }
 
     // end video
 
@@ -160,7 +256,38 @@ export default function Player() {
     return <Holder style={{width: width, height: height}}>
       {media && <>
         <Controls style={{width: width - 48}}>
-          <ControlsHolder></ControlsHolder>
+          <ControlsHolder>
+            <ControlBlock className="left">
+              <Slider
+                min={0}
+                max={1}
+                defaultValue={volume}
+                step={0.01}
+                style={{width: "50%"}}
+                tooltip={{ formatter: displayVolume }}
+                onChange={changeVolume}
+              />
+            </ControlBlock>
+            <ControlBlock>
+              <PlayPauseButton onClick={playPauseAction}>
+                {isPlaying ? <PauseOutlined /> : <CaretRightOutlined /> }
+              </PlayPauseButton>
+            </ControlBlock>
+            <ControlBlock className="right">
+              <ControlBlockButton>CC</ControlBlockButton>
+              <Dropdown
+                placement="top"
+                menu={{
+                  items: playbackRate,
+                  selectable: true,
+                  defaultSelectedKeys: ['1'],
+                  onClick: changePlaybackRate
+                }}
+              >
+                <ControlBlockButton>{currentPlaybackRate}x</ControlBlockButton>
+              </Dropdown>
+            </ControlBlock>
+          </ControlsHolder>
           <ProgressHolder>
             <ProgressCurrent>{sec2Time(currentTime)}</ProgressCurrent>
             <ProgressBar style={{width: width - 248}}>
@@ -170,7 +297,7 @@ export default function Player() {
                 defaultValue={currentTime}
                 style={{width: "100%"}}
                 tooltip={{ formatter: sec2Time }}
-                onChangeComplete={setCurrentTime}
+                onChangeComplete={changeCurrentTime}
               />
             </ProgressBar>
             <ProgressRemaining>{sec2Time(duration - currentTime)}</ProgressRemaining>
